720cc1cfe5be0dc2a2f16af63e55955a92b1b39d
XxTroiaxX <pedro.troia2001@gmail.com>
2019-07-14 21:59:28 +0100

    Breakout

---
+++ Breakout/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Breakout
+{
+    partial class Board
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Time = new System.Windows.Forms.Timer(this.components);
+            this.Plat = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Red;
+            this.Ball.Location = new System.Drawing.Point(271, 277);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(31, 28);
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // Time
+            // 
+            this.Time.Interval = 1;
+            this.Time.Tick += new System.EventHandler(this.Time_Tick);
+            // 
+            // Plat
+            // 
+            this.Plat.BackColor = System.Drawing.Color.Black;
+            this.Plat.Location = new System.Drawing.Point(195, 456);
+            this.Plat.Name = "Plat";
+            this.Plat.Size = new System.Drawing.Size(190, 21);
+            this.Plat.TabIndex = 1;
+            this.Plat.TabStop = false;
+            // 
+            // Board
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(596, 520);
+            this.Controls.Add(this.Plat);
+            this.Controls.Add(this.Ball);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Board";
+            this.Text = "Breakout";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Board_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plat)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer Time;
+        private System.Windows.Forms.PictureBox Plat;
+    }
+}
+
---
+++ Breakout/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Breakout
+{
+    public partial class Board : Form
+    {
+        public Board()
+        {
+            InitializeComponent();
+            Time.Enabled = true;
+            setBlocks();
+        }
+
+        int Vspeed = 2;
+        int Lspeed = 2;
+        public const int row = 5;
+        public const int col = 6;
+        public PictureBox[,] blocks; 
+
+        private void setBlocks()
+        {
+            int blockheight = 25;
+            int blockwidth = 100;
+
+            blocks = new PictureBox[row, col];
+
+            for(int x = 0; x < row; x++)
+            {
+                for(int y = 0; y < col; y++)
+                {
+                    blocks[x, y] = new PictureBox();
+                    blocks[x, y].Width = blockwidth;
+                    blocks[x, y].Height = blockheight;
+                    blocks[x, y].Top = blockheight * x;
+                    blocks[x, y].Left = blockwidth * y;
+                    blocks[x, y].BackColor = Color.Green;
+                    blocks[x, y].BorderStyle = BorderStyle.Fixed3D;
+
+                    this.Controls.Add(blocks[x, y]);
+                }
+            }
+        }
+
+
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            Ball.Top += Vspeed;
+            Ball.Left += Lspeed;
+
+            if(Ball.Bottom == this.ClientRectangle.Bottom)
+            {
+                Time.Stop();
+                MessageBox.Show("Test");
+            }
+            else if(Ball.Top == this.ClientRectangle.Top)
+            {
+                Vspeed = 2;
+            }
+            else if(Ball.Left == this.ClientRectangle.Left)
+            {
+                Lspeed = 2;
+            }
+            else if(Ball.Right == this.ClientRectangle.Right)
+            {
+                Lspeed = -1;
+            }
+            else if(Ball.Bounds.IntersectsWith(Plat.Bounds) == true)
+            {
+                Vspeed = -2;
+            }
+
+            for(int x = 0; x < row; x++)
+            {
+                for(int y = 0; y < col; y++)
+                {
+                    if(Ball.Bounds.IntersectsWith(blocks[x,y].Bounds) == true && blocks[x,y].Visible == true)
+                    {
+                        blocks[x, y].Visible = false;
+                        Vspeed = -Vspeed;
+                    }
+                }
+            }
+        }
+
+        private void Board_MouseMove(object sender, MouseEventArgs e)
+        {
+            Plat.Left = e.X - (Plat.Width / 2);
+        }
+    }
+}
